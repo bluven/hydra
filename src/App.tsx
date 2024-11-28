810c93de188e15fed822e9b80000b1c1490a945f
bluven <yanshiyi1983@163.com>
2024-11-28 15:42:38 +0800

    Add router and Login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/electron-vite.animate.svg'
-import './App.css'
+// import { useState, useEffect } from "react";
+// import { getBrowserLang } from "@/utils/util";
+import { ConfigProvider } from "antd";
+// import { setLanguage } from "@/redux/modules/global";
+import { HashRouter } from "react-router-dom";
+// import { RootState, useDispatch, useSelector } from "@/redux";
+import AuthRouter from "@/routers/utils/authRouter";
+import Router from "@/routers/index";
+import useTheme from "@/hooks/useTheme";
+// import zhCN from "antd/lib/locale/zh_CN";
+// import enUS from "antd/lib/locale/en_US";
+// import i18n from "i18next";
+import "moment/dist/locale/zh-cn";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+	// const dispatch = useDispatch();
+	// const { language, assemblySize } = useSelector((state: RootState) => state.global);
+	// const [i18nLocale, setI18nLocale] = useState(zhCN);
 
-  return (
-    <>
-      <div>
-        <a href="https://electron-vite.github.io" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+	// 全局使用主题
+	useTheme();
 
-export default App
+	// 设置 antd 语言国际化
+	// const setAntdLanguage = () => {
+	// 	// 如果 redux 中有默认语言就设置成 redux 的默认语言，没有默认语言就设置成浏览器默认语言
+	// 	if (language && language == "zh") return setI18nLocale(zhCN);
+	// 	if (language && language == "en") return setI18nLocale(enUS);
+	// 	if (getBrowserLang() == "zh") return setI18nLocale(zhCN);
+	// 	if (getBrowserLang() == "en") return setI18nLocale(enUS);
+	// };
+
+	// useEffect(() => {
+	// 	// 全局使用国际化
+	// 	// i18n.changeLanguage(language || getBrowserLang());
+	// 	dispatch(setLanguage(language || getBrowserLang()));
+	// 	setAntdLanguage();
+	// }, [language]);
+
+	return (
+		<HashRouter>
+			<ConfigProvider>
+				<AuthRouter>
+					<Router />
+				</AuthRouter>
+			</ConfigProvider>
+		</HashRouter>
+	);
+};
+
+export default App;
